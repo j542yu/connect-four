b70ddccdafb3e69367eecf3a73017f74f8f7e52a
judywsl <j542yu@uwaterloo.ca>
2025-02-16 14:47:24 -0500

    Add main.rb to handle initializing new game
    User can choose to play again and start new game
    or exit game

---
+++ lib/main.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require_relative 'game'
+
+print "Hey there, want to play Connect Four? (y/n)\n=> "
+input = nil
+loop do
+  loop do
+    input = gets.chomp
+    break if %w[y n].include?(input)
+
+    puts "\nInvalid input. Type 'y' to play or 'n' to exit."
+  end
+
+  if input == 'y'
+    puts "\nGreat! Here we go!"
+    Game.new.play
+  else
+    puts "\nOkay... Byebye :("
+    exit
+  end
+
+  print "\nPlay again? (y/n)\n=> "
+end
